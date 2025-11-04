8d0ab8f1c54b2d0ecdfd6366e4c2ceda7f6d3c6c
Shaveika Kseniya <ksushashaveyko@gmail.com>
2025-11-04 18:48:31 +0300

    Add LR1

---
+++ STRWEB/LR1/real_estate_agency/catalog/admin.py
@@ -0,0 +1,75 @@
+from django.contrib import admin
+
+from .models import Service, Estate, Sale, ServiceCategory, PurchaseRequest
+
+
+class EstateInline(admin.TabularInline):
+    model = Estate
+
+
+class SaleInline(admin.TabularInline):
+    model = Sale
+
+
+class CategoryInline(admin.TabularInline):
+    model = Service
+
+
+@admin.register(ServiceCategory)
+class ServiceCategoryAdmin(admin.ModelAdmin):
+    inlines = (CategoryInline,)
+
+
+@admin.register(Service)
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [EstateInline]
+
+
+@admin.register(Estate)
+class EstateAdmin(admin.ModelAdmin):
+    list_display = ["address", "cost", "category", "sale"]
+    list_filter = ["category", "category__category"]
+
+
+@admin.register(PurchaseRequest)
+class PurchaseRequestAdmin(admin.ModelAdmin):
+    list_display = ["client", "estate", "created_at", "status"]
+
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = [
+        "employee",
+        "date_of_sale",
+        "estate",
+        "cost",
+        "get_estate_category",
+    ]
+    list_filter = ["date_of_sale", "employee", "estate__category"]
+
+    def get_estate_category(self, obj):
+        return obj.estate.category
+    get_estate_category.short_description = "Estate Category"
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "employee",
+                    "client",
+                )
+            },
+        ),
+        (
+            "Estate",
+            {
+                "fields": (
+                    "estate",
+                    "date_of_sale",
+                    "date_of_contract",
+                )
+            },
+        ),
+    )
+
